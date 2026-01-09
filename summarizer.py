019b538b3a508c0c7600897e95cd89118f3f52bd
tushar-2301 <tushar.chinchanikar2@gmail.com>
2026-01-09 10:45:21 +0530

    Create summarizer.py

---
+++ summarizer.py
@@ -0,0 +1,22 @@
+from google import genai
+import os
+from newsletter_html import books
+
+def prompt_maker():
+    return f"""
+    You are being given some data about a book.
+    Title: {books[0]['book_title']}
+    Price: £{books[0]['book_price_num']}
+    Rating: {books[0]['book_star_num']} out of 5
+    Stock: {books[0]['book_stock']}
+    Scraped at: {books[0]['scraped_at']}
+
+    Write a concise, neutral summary in 4 sentences.
+    """
+
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt_maker())
+
+print(response.text)
